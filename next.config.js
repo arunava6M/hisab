591f4219d1dce586f635326b4580211eade60a81
ArunavaM <arunava6mondal@gmail.com>
2024-07-10 20:44:03 +0530

    pwa next config

--- next.config.js
+++ next.config.js
@@ -18,8 +18,6 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  distDir: "proj",
-  output: "export",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.plugins.push(
@@ -49,7 +47,7 @@ const nextConfig = {
 // Configuration object tells the next-pwa plugin
 const withPWA = require("next-pwa")({
   dest: "public", // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  // disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
 });
