2449aeb3f940d9d9a02ce2bc377a0dc409a4d16f
ArunavaM <arunava6mondal@gmail.com>
2024-07-10 21:35:13 +0530

    Capacitor working config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,13 @@ const { GenerateSW } = require("workbox-webpack-plugin");
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  output: "export",
+  distDir: "out",
+  // assetPrefix: isProd ? "/hisab-app/" : "",
+  // swcMinify: true,
   compiler: {
     styledComponents: true,
-    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    // removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
   // googleFonts: {
   //   fonts: [{ family: "Roboto", variants: ["400", "500", "700"] }],
@@ -18,39 +21,40 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.plugins.push(
-        new GenerateSW({
-          clientsClaim: true,
-          skipWaiting: true,
-          swDest: "public/sw.js",
-          runtimeCaching: [
-            {
-              urlPattern: /^https?.*/,
-              handler: "NetworkFirst",
-              options: {
-                cacheName: "offlineCache",
-                expiration: {
-                  maxEntries: 200,
-                },
-              },
-            },
-          ],
-        })
-      );
-    }
-    return config;
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.plugins.push(
+  //       new GenerateSW({
+  //         clientsClaim: true,
+  //         skipWaiting: true,
+  //         swDest: "public/sw.js",
+  //         runtimeCaching: [
+  //           {
+  //             urlPattern: /^https?.*/,
+  //             handler: "NetworkFirst",
+  //             options: {
+  //               cacheName: "offlineCache",
+  //               expiration: {
+  //                 maxEntries: 200,
+  //               },
+  //             },
+  //           },
+  //         ],
+  //       })
+  //     );
+  //   }
+  //   return config;
+  // },
 };
 
 // Configuration object tells the next-pwa plugin
-const withPWA = require("next-pwa")({
-  dest: "public", // Destination directory for the PWA files
-  // disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-  register: true, // Register the PWA service worker
-  skipWaiting: true, // Skip waiting for service worker activation
-});
+// const withPWA = require("next-pwa")({
+//   dest: "public", // Destination directory for the PWA files
+//   // disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+//   register: true, // Register the PWA service worker
+//   skipWaiting: true, // Skip waiting for service worker activation
+// });
 
-// Export the combined configuration for Next.js with PWA support
-module.exports = withPWA(nextConfig);
+// // Export the combined configuration for Next.js with PWA support
+// module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
